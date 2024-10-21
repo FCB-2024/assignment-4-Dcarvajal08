d280e879fde6c3986028bfecb5e503f3a3ea126b
Dcarvajal08 <diego.carvajal01@estudiant.upf.edu>
2024-10-21 20:09:58 +0200

    cambio2

--- src/antiprime.py
+++ src/antiprime.py
@@ -4,40 +4,27 @@
 import sys
 def main() :
 	## YOU CODE SHOULD START HERE AST THE SAME
-	## IDENTATION AS THIS COMMENT
-	x=int(sys.argv([1]))	
-	def divisorsx():
+	## IDENTATION AS THIS COMMENT	
+	x=int(sys.argv[1])
+	def divisorsx(x):
 		divisorsx=0
 		comptadorx=1
 		while comptadorx<=x:
-				if x%comptadorx==0:
-					divisorsx=divisorsx+1
-		comptadorx=comptadorx+1
-		return(divisorsx)
-	r1=divisorsx
- 
-	def divisorspetitsx():
-		comptadorx=1
-		divisorspetitsx=0
-		while comptadorx<x:
-			comptadorpetitsx=1
-			while comptadorpetitsx<comptadorx:
-				if comptadorx % comptadorpetitsx ==0:
-					divisorspetitsx=divisorspetitsx+1
-		comptadorx=comptadorx+1
-		comptadorpetitsx=comptadorpetitsx+1
-		return(divisorspetitsx)
-	r2=divisorspetitsx
+			if x%comptadorx==0:
+				divisorsx=divisorsx+1
+			comptadorx=comptadorx+1
+		return divisorsx
+	r1=divisorsx(x)
+	for comptadorx in range(1,x):
+		if divisorsx(comptadorx)>=r1:
+			return "not anti-prime"
+	return "anti-prime"
+	comptadorx=comptadorx+1
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	if r1>=r2:
-			return("Not antiprime")
-	else:
-			return("Anti-prime")
-
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
 
